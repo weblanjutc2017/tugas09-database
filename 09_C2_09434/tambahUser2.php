20a3b7f2d0b8f796d9229c9b4d62a4f9bd78b1d0
Abdurrahman Trimanto <capteinn@gmail.com>
2017-09-19 19:25:09 +0700

    merapikan format coding pada tambahUser2.php

--- 09_C2_09434/tambahUser2.php
+++ 09_C2_09434/tambahUser2.php
@@ -1,23 +1,26 @@
 <?php
-$host = "localhost"; // Host name
-$username = "root"; // Mysql username
-$password = ""; // Mysql password
-$db_name = "praktikum8"; // Database name
-$conn = mysqli_connect($host, $username, $password, $db_name);
-// Check connection
-if (!$conn) {
-die("Connection failed: " . mysqli_connect_error());
-}
-$username = $_POST['username'];
-$password = $_POST['password'];
-$level = $_POST['level'];
-$input = "insert into user(username, password, level) values ('$username', '$password','$level')";
-if ($username == "" or $password == ""){
-echo '<script type="text/javascript">
-var answer = alert("Data masih belum lengkap") window.location = "tambahUser.php";
-</script>';
-}else{
-$hasil = mysqli_query($conn, $input);
-header('location:index.php?update=2');
-}
+	$host = "localhost"; // Host name
+	$username = "root"; // Mysql username
+	$password = ""; // Mysql password
+	$db_name = "praktikum8"; // Database name
+	$conn = mysqli_connect($host, $username, $password, $db_name);
+	
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$level = $_POST['level'];
+	$input = "insert into user(username, password, level) values ('$username', '$password','$level')";
+	
+	if ($username == "" or $password == ""){
+		echo '<script type="text/javascript">
+			var answer = alert("Data masih belum lengkap") window.location = "tambahUser.php";
+			</script>';
+	}else{
+		$hasil = mysqli_query($conn, $input);
+		header('location:index.php?update=2');
+	}
 ?>
