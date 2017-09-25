aa5d963b061cdf3efb38b123228506417dbf5783
jogjat <JTAnnashobari@gmail.com>
2017-09-25 23:15:42 +0700

    mengumpulkan tugas 09

---
+++ 09_C2_10493/tambahUser2.php
@@ -0,0 +1,17 @@
+<?php
+require_once'koneksi.php';
+$username = $_POST['username'];
+$password = $_POST['password'];
+$level = $_POST['level'];
+$input = "insert into user(username, password, level) values ('$username', '$password',
+'$level')";
+if ($username == "" or $password == ""){
+ echo '<script type="text/javascript">
+ var answer = alert("Data masih belum lengkap")
+ window.location = "tambahUser.php";
+ </script>';
+}else{
+ $hasil = mysqli_query($conn, $input);
+ header('location:index.php?update=2');
+}
+?>
