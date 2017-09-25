d479bf10212d57072aad97eab388337dab885150
achisrizal <achisrizal@gmail.com>
2017-09-25 22:01:05 +0700

    09_C2_08835

---
+++ 09_C2_08835/tambahUser.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Bootstrap -->
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="asset/css/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="span2"></div>
+				<div class="span8">
+					<center>
+						<h3>TAMBAH DATA USER</h3>
+						<form action="tambahUser2.php" method="post">
+							<table width="319" border="0">
+								<tr>
+									<td width="152">Username</td>
+									<td width="157"><input name="username" type="text" id="username" size="20" /></td>
+								</tr>
+								<tr>
+									<td>Password</td>
+									<td><input name="password" type="password" id="password" size="20" /></td>
+								</tr>
+								<tr>
+									<td>Level</td>
+									<td>
+										<select name='level'>
+											<option value='user'>User</option>
+											<option value='admin'>Admin</option>
+										</select>
+									</td>
+								</tr>
+							</table>
+							<br/>
+							<table width border="0">
+								<tr>
+								<td width="92"><button class="btn btnwarning" type="submit">Simpan</a></button></td>
+								<td width="92"><button><a href="index.php" class="btn btn-warning" type="button">Batal</a></button></td>
+								</tr>
+							</table>
+						</form>
+					</center>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ 09_C2_08835/tambahUser2.php
@@ -0,0 +1,33 @@
+<?php
+	$host = "localhost"; // Host name
+	$username = "root"; // Mysql username
+	$password = ""; // Mysql password
+	$db_name = "praktikum8"; // Database name
+	$conn = mysqli_connect($host, $username, $password, $db_name);
+	
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$level = $_POST['level'];
+	$input = "insert into user(username, password, level) values ('$username', '$password','$level')";
+	$cek = "select username from user where username='$username'";
+	$check = mysqli_query($conn, $cek);
+	$row = mysqli_fetch_array($check);
+	
+	if ($username == "" or $password == ""){
+		echo '<script type="text/javascript">
+			alert("Data masih belum lengkap"); window.location = "tambahUser.php";
+			</script>';
+	}elseif($username == $row['username']){
+		echo '<script type="text/javascript">
+			alert("Data telah digunakan"); window.location = "tambahUser.php";
+			</script>';
+	}else{
+		$hasil = mysqli_query($conn, $input);
+		header('location:index.php?update=2');
+	}
+?>
